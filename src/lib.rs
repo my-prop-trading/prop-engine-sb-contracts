920ec2febc3a21826e9f0531f3c84f64fd219e76
Aleksandr Belousov <aleksandr.b@simple.app>
2023-04-19 00:52:17 +0300

    fix feature account-registration

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
-#[cfg(feature = "account_registration")]
+#[cfg(feature = "account-registration")]
 mod account_registration;
 
-#[cfg(feature = "account_registration")]
+#[cfg(feature = "account-registration")]
 pub use account_registration::*;
