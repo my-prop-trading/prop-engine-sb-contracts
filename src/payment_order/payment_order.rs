f970c0e8aff07902b518d0c6e96fcedb909ecc0b
Aleksandr Belousov <aleksandr.b@simple.app>
2023-06-05 15:23:33 +0300

    update event

--- src/payment_order/payment_order.rs
+++ src/payment_order/payment_order.rs
@@ -2,7 +2,7 @@
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "payment-order")]
 pub struct PaymentOrderSbModel {
     #[prost(message, tag = "1")]
-    pub event: Option<RegisteredSbModel>,
+    pub event: Option<PaymentOrderBodySbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
