1d4fe9043b5d70ddebc476647cfef56ad49bdb9a
Aleksandr Belousov <aleksandr.b@simple.app>
2023-06-18 22:25:06 +0300

    updated

--- src/account_balance_update/account_balance_update.rs
+++ src/account_balance_update/account_balance_update.rs
@@ -2,17 +2,15 @@
 #[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "account-balance-update")]
 pub struct AccountBalanceUpdateSbModel {
     #[prost(message, tag = "1")]
-    pub event: Option<TAccountBalanceUpdateBodySbModel>,
+    pub event: Option<AccountBalanceUpdateBodySbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct TAccountBalanceUpdateBodySbModel {
+pub struct AccountBalanceUpdateBodySbModel {
     #[prost(string, tag = "1")]
     pub trader_account_id: String,
     #[prost(int64, tag = "2")]
     pub updated_at: i64,
-    #[prost(int64, tag = "3")]
-    pub leverage: i64,
     #[prost(double, tag = "4")]
     pub balance: f64,
     #[prost(double, tag = "5")]
