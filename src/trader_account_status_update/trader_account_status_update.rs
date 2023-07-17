d054dd2e98fd191c9237385faab0f6e9b657e608
Aleksandr Belousov <aleksandr.b@simple.app>
2023-07-17 15:50:40 +0300

    https://smplt.atlassian.net/browse/PROPOURKO-462 https://smplt.atlassian.net/browse/PROPOURKO-463

--- src/trader_account_status_update/trader_account_status_update.rs
+++ src/trader_account_status_update/trader_account_status_update.rs
@@ -1,8 +1,8 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
-#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "trader-account-creation")]
+#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "trader-account-status-update")]
 pub struct TraderAccountStatusUpdateSbModel {
     #[prost(message, tag = "1")]
-    pub event: Option<TraderAccountCreatedBodySbModel>,
+    pub event: Option<TraderAccountStatusUpdateBodySbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -10,22 +10,34 @@ pub struct TraderAccountStatusUpdateBodySbModel {
     #[prost(string, tag = "1")]
     pub trader_account_id: String,
 
-    #[prost(enumeration="TradingPlatformDto", tag = "2")]
+    #[prost(enumeration="TradingPlatform", tag = "2")]
     pub trading_platform_type: i32,
 
-    #[prost(enumeration="BrokerDto", tag = "3")]
+    #[prost(enumeration="Broker", tag = "3")]
     pub broker_type: i32,
+
+    #[prost(enumeration="TraderAccountStatus", tag = "4")]
+    pub status: i32 
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
-pub enum TradingPlatformDto {
+pub enum TradingPlatform {
     MetaTrader4 = 0,    
     MetaTrader5 = 1,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
-pub enum BrokerDto {
+pub enum Broker {
     Welltrade = 0,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum TraderAccountStatus {
+    New = 0,
+    Active = 1,
+    Disabled = 2,
+    StageCompleted = 3,
 }
