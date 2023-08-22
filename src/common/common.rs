9895466895c38b3b5dfcc4ad295eaac5b6df2461
Aleksandr Belousov <aleksandr.b@simple.app>
2023-08-22 20:47:30 +0300

    fixed

--- src/common/common.rs
+++ src/common/common.rs
@@ -1,34 +1,3 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
-#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "trader-account-status-update")]
-pub struct TraderAccountStatusUpdateSbModel {
-    #[prost(message, tag = "1")]
-    pub event: Option<TraderAccountStatusUpdateBodySbModel>,
-}
-
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct TraderAccountStatusUpdateBodySbModel {
-    #[prost(string, tag = "1")]
-    pub trader_account_id: String,
-
-    #[prost(enumeration="TradingPlatform", tag = "2")]
-    pub trading_platform_type: i32,
-
-    #[prost(enumeration="Broker", tag = "3")]
-    pub broker_type: i32,
-
-    #[prost(enumeration="TraderAccountStatus", tag = "4")]
-    pub status: i32 ,
-
-    #[prost(string, tag = "5")]
-    pub brand: String,
-
-    #[prost(string, tag = "6")]
-    pub client_id: String,
-
-    #[prost(int32, tag = "7")]
-    pub phase: i32,
-}
-
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum TradingPlatform {
@@ -39,14 +8,6 @@ pub enum TradingPlatform {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum Broker {
-    Welltrade = 0,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-#[repr(i32)]
-pub enum TraderAccountStatus {
-    New = 0,
-    Active = 1,
-    Disabled = 2,
-    StageCompleted = 3,
+    WelltradeDemo = 0,
+    WelltradeLive = 1,
 }
