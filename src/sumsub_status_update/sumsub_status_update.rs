c8e20cd291227420ec6dc658525d0c063b722882
yuriy-smplt <yuriy.p@smplt.net>
2023-09-28 18:02:03 +0300

    updated sumsub

--- src/sumsub_status_update/sumsub_status_update.rs
+++ src/sumsub_status_update/sumsub_status_update.rs
@@ -1,38 +1,77 @@
-service_sdk::macros::use_my_sb_entity_protobuf_model!();
-pub use crate::completed_status_model::CompletedStatusSbModel;
-
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-#[my_sb_entity_protobuf_model(topic_id = "sumsub-status-update")]
 pub struct SumsubUpdateSbModel {
-    #[prost(message, tag = "1")]
-    pub event: Option<SumsubUpdateBodySbModel>,
+    #[prost(oneof = "sumsub_update_sb_model::Response", tags = "1, 2, 3")]
+    pub response: ::core::option::Option<sumsub_update_sb_model::Response>,
 }
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-#[repr(i32)]
-pub enum SumsubReviewStatus {
-    Init = 0,
-    Pending = 1,
-    Prechecked = 2,
-    Queued = 3,
-    Completed = 4,
-    OnHold = 5,
+/// Nested message and enum types in `SumsubUpdateSbModel`.
+pub mod sumsub_update_sb_model {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Response {
+        #[prost(message, tag = "1")]
+        Updated(super::UpdatedSbModel),
+        #[prost(message, tag = "2")]
+        Successful(super::CompletedSuccessfulSbModel),
+        #[prost(message, tag = "3")]
+        Rejected(super::CompletedRejectedSbModel),
+    }
 }
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-#[repr(i32)]
-pub enum SumsubProofType {
-    ProofOfAddress = 0,
-    ProofOfIdentity = 1,
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CompletedRejectedSbModel {
+    #[prost(message, optional, tag = "1")]
+    pub body: ::core::option::Option<SumsubUpdateBodySbModel>,
+    #[prost(enumeration = "SumsubReviewRejectStatus", tag = "2")]
+    pub reject_status: i32,
 }
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-#[repr(i32)]
-pub enum SumsubReviewRejectStatus {
-    Final = 0,
-    Retry = 1,
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CompletedSuccessfulSbModel {
+    #[prost(message, optional, tag = "1")]
+    pub body: ::core::option::Option<SumsubUpdateBodySbModel>,
 }
-
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct UpdatedSbModel {
+    #[prost(message, optional, tag = "1")]
+    pub body: ::core::option::Option<SumsubUpdateBodySbModel>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct SumsubUpdateBodySbModel {
+    #[prost(string, tag = "1")]
+    pub client_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub verification_id: ::prost::alloc::string::String,
+    #[prost(string, tag = "3")]
+    pub applicant_id: ::prost::alloc::string::String,
+    #[prost(int64, tag = "4")]
+    pub created_at: i64,
+    #[prost(enumeration = "SumsubNotificationType", tag = "5")]
+    pub notification_type: i32,
+    #[prost(enumeration = "SumsubProofType", tag = "6")]
+    pub proof_type: i32,
+    #[prost(enumeration = "SumsubReviewStatus", tag = "7")]
+    pub review_status: i32,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CompletedStatusSbModel {
+    #[prost(oneof = "completed_status_sb_model::Response", tags = "1, 2")]
+    pub response: ::core::option::Option<completed_status_sb_model::Response>,
+}
+/// Nested message and enum types in `CompletedStatusSbModel`.
+pub mod completed_status_sb_model {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Response {
+        #[prost(message, tag = "1")]
+        Successful(super::CompletedSuccessfulSbModel),
+        #[prost(message, tag = "2")]
+        Rejected(super::CompletedRejectedSbModel),
+    }
+}
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum SumsubNotificationType {
@@ -52,94 +91,139 @@ pub enum SumsubNotificationType {
     TravelRuleStatusChanged = 13,
     WorkflowCompleted = 14,
 }
-
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct CompletedSuccessfulSbModel {}
-
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct CompletedRejectedSbModel {
-    #[prost(enumeration = "SumsubReviewRejectStatus", tag = "1")]
-    pub reject_status: i32,
-}
-
-pub mod completed_status_model {
-    #[allow(clippy::derive_partial_eq_without_eq)]
-    #[derive(Clone, PartialEq, ::prost::Oneof)]
-    pub enum CompletedStatusSbModel {
-        #[prost(message, tag = "1")]
-        Successful(super::CompletedSuccessfulSbModel),
-        #[prost(message, tag = "2")]
-        Rejected(super::CompletedRejectedSbModel),
+impl SumsubNotificationType {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            SumsubNotificationType::Reviewed => "Reviewed",
+            SumsubNotificationType::Pending => "Pending",
+            SumsubNotificationType::Created => "Created",
+            SumsubNotificationType::OnHold => "OnHold",
+            SumsubNotificationType::PersonalInfoChanged => "PersonalInfoChanged",
+            SumsubNotificationType::Prechecked => "Prechecked",
+            SumsubNotificationType::Deleted => "Deleted",
+            SumsubNotificationType::LevelChanged => "LevelChanged",
+            SumsubNotificationType::VideoIdentStatusChanged => "VideoIdentStatusChanged",
+            SumsubNotificationType::Reset => "Reset",
+            SumsubNotificationType::ActionPending => "ActionPending",
+            SumsubNotificationType::ActionReviewed => "ActionReviewed",
+            SumsubNotificationType::ActionOnHold => "ActionOnHold",
+            SumsubNotificationType::TravelRuleStatusChanged => "TravelRuleStatusChanged",
+            SumsubNotificationType::WorkflowCompleted => "WorkflowCompleted",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Reviewed" => Some(Self::Reviewed),
+            "Pending" => Some(Self::Pending),
+            "Created" => Some(Self::Created),
+            "OnHold" => Some(Self::OnHold),
+            "PersonalInfoChanged" => Some(Self::PersonalInfoChanged),
+            "Prechecked" => Some(Self::Prechecked),
+            "Deleted" => Some(Self::Deleted),
+            "LevelChanged" => Some(Self::LevelChanged),
+            "VideoIdentStatusChanged" => Some(Self::VideoIdentStatusChanged),
+            "Reset" => Some(Self::Reset),
+            "ActionPending" => Some(Self::ActionPending),
+            "ActionReviewed" => Some(Self::ActionReviewed),
+            "ActionOnHold" => Some(Self::ActionOnHold),
+            "TravelRuleStatusChanged" => Some(Self::TravelRuleStatusChanged),
+            "WorkflowCompleted" => Some(Self::WorkflowCompleted),
+            _ => None,
+        }
     }
 }
-
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct SumsubUpdateBodySbModel {
-    #[prost(string, tag = "1")]
-    pub client_id: String,
-    #[prost(string, tag = "2")]
-    pub verification_id: String,
-    #[prost(string, tag = "3")]
-    pub applicant_id: String,
-    #[prost(int64, tag = "4")]
-    pub created_at: i64,
-    #[prost(enumeration = "SumsubNotificationType", tag = "5")]
-    pub notification_type: i32,
-    #[prost(enumeration = "SumsubProofType", tag = "6")]
-    pub proof_type: i32,
-    #[prost(enumeration = "SumsubReviewStatus", tag = "7")]
-    pub review_status: i32,
-    #[prost(oneof = "completed_status_model::CompletedStatusSbModel", tags = "8, 9")]
-    pub completed_status: Option<completed_status_model::CompletedStatusSbModel>,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum SumsubReviewRejectStatus {
+    Final = 0,
+    Retry = 1,
 }
-
-pub fn i32_to_sumsub_review_status(value: i32) -> SumsubReviewStatus {
-    match value {
-        0 => SumsubReviewStatus::Init,
-        1 => SumsubReviewStatus::Pending,
-        2 => SumsubReviewStatus::Prechecked,
-        3 => SumsubReviewStatus::Queued,
-        4 => SumsubReviewStatus::Completed,
-        5 => SumsubReviewStatus::OnHold,
-        _ => panic!("Unknown SumsubReviewStatus: {}", value),
+impl SumsubReviewRejectStatus {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            SumsubReviewRejectStatus::Final => "Final",
+            SumsubReviewRejectStatus::Retry => "Retry",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Final" => Some(Self::Final),
+            "Retry" => Some(Self::Retry),
+            _ => None,
+        }
     }
 }
-
-pub fn i32_to_sumsub_notification_type(value: i32) -> SumsubNotificationType {
-    match value {
-        0 => SumsubNotificationType::Reviewed,
-        1 => SumsubNotificationType::Pending,
-        2 => SumsubNotificationType::Created,
-        3 => SumsubNotificationType::OnHold,
-        4 => SumsubNotificationType::PersonalInfoChanged,
-        5 => SumsubNotificationType::Prechecked,
-        6 => SumsubNotificationType::Deleted,
-        7 => SumsubNotificationType::LevelChanged,
-        8 => SumsubNotificationType::VideoIdentStatusChanged,
-        9 => SumsubNotificationType::Reset,
-        10 => SumsubNotificationType::ActionPending,
-        11 => SumsubNotificationType::ActionReviewed,
-        12 => SumsubNotificationType::ActionOnHold,
-        13 => SumsubNotificationType::TravelRuleStatusChanged,
-        14 => SumsubNotificationType::WorkflowCompleted,
-        _ => panic!("Unknown SumsubNotificationType: {}", value),
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum SumsubProofType {
+    ProofOfAddress = 0,
+    ProofOfIdentity = 1,
+}
+impl SumsubProofType {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            SumsubProofType::ProofOfAddress => "ProofOfAddress",
+            SumsubProofType::ProofOfIdentity => "ProofOfIdentity",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "ProofOfAddress" => Some(Self::ProofOfAddress),
+            "ProofOfIdentity" => Some(Self::ProofOfIdentity),
+            _ => None,
+        }
     }
 }
-
-pub fn i32_to_sumsub_proof_type(value: i32) -> SumsubProofType {
-    match value {
-        0 => SumsubProofType::ProofOfAddress,
-        1 => SumsubProofType::ProofOfIdentity,
-        _ => panic!("Unknown SumsubProofType: {}", value),
-    }
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum SumsubReviewStatus {
+    Init = 0,
+    Pending = 1,
+    Prechecked = 2,
+    Queued = 3,
+    Completed = 4,
+    OnHold = 5,
 }
-
-pub fn i32_to_sumsub_review_reject_status(value: i32) -> SumsubReviewRejectStatus {
-    match value {
-        0 => SumsubReviewRejectStatus::Final,
-        1 => SumsubReviewRejectStatus::Retry,
-        _ => panic!("Unknown SumsubReviewRejectStatus: {}", value),
+impl SumsubReviewStatus {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            SumsubReviewStatus::Init => "Init",
+            SumsubReviewStatus::Pending => "Pending",
+            SumsubReviewStatus::Prechecked => "Prechecked",
+            SumsubReviewStatus::Queued => "Queued",
+            SumsubReviewStatus::Completed => "Completed",
+            SumsubReviewStatus::OnHold => "OnHold",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Init" => Some(Self::SrsInit),
+            "Pending" => Some(Self::SrsPending),
+            "Prechecked" => Some(Self::SrsPrechecked),
+            "Queued" => Some(Self::SrsQueued),
+            "Completed" => Some(Self::SrsCompleted),
+            "OnHold" => Some(Self::SrsOnHold),
+            _ => None,
+        }
     }
 }
