124f4315da125aec3a403a42f04b100caa28b645
Aleksandr Belousov <aleksandr.b@simple.app>
2023-11-02 18:18:29 +0200

    https://fintechb2b.atlassian.net/browse/MVPS-311 https://fintechb2b.atlassian.net/browse/MVPS-310 https://fintechb2b.atlassian.net/browse/MVPS-287

---
+++ src/trader_account_blocked/mod.rs
@@ -0,0 +1,3 @@
+mod trader_account_blocked;
+
+pub use trader_account_blocked::*;
---
+++ src/trader_account_blocked/trader_account_blocked.rs
@@ -0,0 +1,28 @@
+use crate::common::TradingPlatform; 
+use crate::common::Broker;
+
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_sb_entity_protobuf_model(topic_id = "trader-account-blocked")]
+pub struct TraderAccountBlockedSbModel {
+    #[prost(message, tag = "1")]
+    pub event: Option<TraderAccountBlockedBodySbModel>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TraderAccountBlockedBodySbModel {
+    #[prost(string, tag = "1")]
+    pub trader_account_id: String,
+
+    #[prost(enumeration="BlockType", tag = "2")]
+    pub Block_type: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum BlockType {
+    Empty = 0,
+    BlockTrades = 1,
+    BlockTradesAndFuture = 2,
+    BlockTradesAndFutureAndCurrent = 4,
+}
