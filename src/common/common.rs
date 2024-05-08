73cf904feb656ed23ffd39380883aa400e6a24ed
yuriy-smplt <yuriy.p@smplt.net>
2024-05-08 16:10:31 +0300

    updated

--- src/common/common.rs
+++ src/common/common.rs
@@ -4,3 +4,18 @@ pub enum AccountType {
     Demo = 0,
     Live = 1,
 }
+
+impl AccountType {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::Demo => "Demo",
+            Self::Live => "live",
+        }
+    }
+}
+
+impl ToString for AccountType {
+    fn to_string(&self) -> String {
+        self.as_str().to_string()
+    }
+}
