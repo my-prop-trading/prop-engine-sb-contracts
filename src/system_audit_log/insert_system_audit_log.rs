535f81e6d8c7b49f47f81c43e17a23b137024f4c
Maxim Gorin <onlinexmax@gmail.com>
2024-08-21 13:07:44 +0300

    Fix build

--- src/system_audit_log/insert_system_audit_log.rs
+++ src/system_audit_log/insert_system_audit_log.rs
@@ -1,4 +1,4 @@
-use service_sdk::my_service_bus::macros::my_sb_entity_protobuf_model;
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
 
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[my_sb_entity_protobuf_model(topic_id = "insert-system-audit-log-command")]
@@ -9,12 +9,12 @@ pub struct InsertSystemAuditLogSbModel {
     pub author: String,
     #[prost(string, tag = "3")]
     pub description: String,
-    #[prost(string, tag = "4")]
+    #[prost(int64, tag = "4")]
     pub timestamp: i64,
     #[prost(string, tag = "5")]
     pub ref_id: String,
     #[prost(string, tag = "6")]
-    pub snapshot_before_update: String,
+    pub snapshot_before: Option<String>,
     #[prost(string, tag = "7")]
-    pub snapshot_after_update: Option<String>,
+    pub snapshot_after: Option<String>,
 }
