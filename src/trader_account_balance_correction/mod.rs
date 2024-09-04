1ba1e1a7cc1fb453559b8d7c14e2c97a4e22411a
Aleksandr Belousov <aleksandr.b@simple.app>
2024-09-04 03:03:10 +0300

    https://fintechb2b.atlassian.net/browse/PROP2024-996

---
+++ src/trader_account_balance_correction/mod.rs
@@ -0,0 +1,3 @@
+mod trader_account_balance_correction;
+
+pub use trader_account_balance_correction::*;
---
+++ src/trader_account_balance_correction/trader_account_balance_correction.rs
@@ -0,0 +1,33 @@
+use crate::common::AccountType; 
+
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_sb_entity_protobuf_model(topic_id = "trader-account-balance-correction")]
+pub struct TraderAccountBalanceCorrectionSbModel {
+    #[prost(message, tag = "1")]
+    pub event: Option<TraderAccountBalanceCorrectionBodySbModel>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct TraderAccountBalanceCorrectionBodySbModel {
+    #[prost(string, tag = "1")]
+    pub trader_account_id: String,
+
+    #[prost(int32, tag = "2")]
+    pub platform_id: i32,
+
+    #[prost(enumeration = "AccountType", tag = "3")]
+    pub account_type: i32,
+
+    #[prost(string, tag = "4")]
+    pub platform_account_id: String,
+
+    #[prost(string, tag = "5")]
+    pub balance_correction_id: String,
+
+    #[prost(double, tag = "6")]
+    pub amount: f64,
+
+    #[prost(string, tag = "7")]
+    pub currency: String,
+}
