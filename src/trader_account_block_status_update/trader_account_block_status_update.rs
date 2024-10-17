8baebab99dbc68097449aafffdac659561230e47
yuriy-smplt <pluriw@gmail.com>
2024-10-17 09:38:23 +0300

    fix naming

--- src/trader_account_block_status_update/trader_account_block_status_update.rs
+++ src/trader_account_block_status_update/trader_account_block_status_update.rs
@@ -7,7 +7,7 @@ pub struct TraderAccountBlockStatusUpdateSbModel {
     #[prost(string, tag = "1")]
     pub trader_account_id: String,
 
-    #[prost(enumeration="BlockType", tag = "2")]
+    #[prost(enumeration="BlockingType", tag = "2")]
     pub block_type: i32,
 
     #[prost(string, tag = "3")]
@@ -22,42 +22,42 @@ pub struct TraderAccountBlockStatusUpdateSbModel {
     #[prost(int32, tag = "6")]
     pub platform_id: i32,
 
-    #[prost(enumeration = "BlockStatus", tag = "7")]
+    #[prost(enumeration = "BlockingStatus", tag = "7")]
     pub block_status: i32,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
-pub enum BlockType {
+pub enum BlockingType {
     Empty = 0,
     BlockTrades = 1,
     BlockTradesAndFuture = 2,
     BlockTradesAndFutureAndCurrent = 4,
 }
 
-impl BlockType {
+impl BlockingType {
     pub fn as_str_name(&self) -> &'static str {
         match self {
-            BlockType::Empty => "Empty",
-            BlockType::BlockTrades => "BlockTrades",
-            BlockType::BlockTradesAndFuture => "BlockTradesAndFuture",
-            BlockType::BlockTradesAndFutureAndCurrent => "BlockTradesAndFutureAndCurrent",
+            BlockingType::Empty => "Empty",
+            BlockingType::BlockTrades => "BlockTrades",
+            BlockingType::BlockTradesAndFuture => "BlockTradesAndFuture",
+            BlockingType::BlockTradesAndFutureAndCurrent => "BlockTradesAndFutureAndCurrent",
         }
     }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
-pub enum BlockStatus {
+pub enum BlockingStatus {
     Blocked = 0,
     Unblocked = 1,
 }
 
-impl BlockStatus {
+impl BlockingStatus {
     pub fn as_str_name(&self) -> &'static str {
         match self {
-            BlockStatus::Blocked => "Blocked",
-            BlockStatus::Unblocked => "Unblocked",
+            BlockingStatus::Blocked => "Blocked",
+            BlockingStatus::Unblocked => "Unblocked",
         }
     }
 }
