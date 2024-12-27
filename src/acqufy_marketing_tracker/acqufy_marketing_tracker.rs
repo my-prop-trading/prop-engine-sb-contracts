a871da5cd41339fbbfb7e27ebbd044c698db238b
yuriy-smplt <pluriw@gmail.com>
2024-12-27 18:09:36 +0200

    updated

--- src/acqufy_marketing_tracker/acqufy_marketing_tracker.rs
+++ src/acqufy_marketing_tracker/acqufy_marketing_tracker.rs
@@ -3,14 +3,17 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 #[my_sb_entity_protobuf_model(topic_id = "acqufy-marketing-tracker")]
 pub struct AcqufyMarketingTrackerSbModel {
     #[prost(string, tag = "1")]
-    pub click_id: String,
+    pub ref_id: String,
 
     #[prost(string, tag = "2")]
-    pub user_agent: String,
+    pub click_id: String,
 
     #[prost(string, tag = "3")]
+    pub user_agent: String,
+
+    #[prost(string, tag = "4")]
     pub ip: String,
 
-    #[prost(map = "string, string", tag = "4")]
+    #[prost(map = "string, string", tag = "5")]
     pub parameters: std::collections::HashMap<String, String>,
 }
