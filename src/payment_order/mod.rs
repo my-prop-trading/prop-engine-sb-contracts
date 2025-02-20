2ea3bef6e98bd0fc6015b4a19d2a31b6a1383be9
Maxim Gorin <onlinexmax@gmail.com>
2025-02-20 19:28:08 +0300

    Add payment order received

--- src/payment_order/mod.rs
+++ src/payment_order/mod.rs
@@ -1,3 +1,6 @@
 mod payment_order;
 
 pub use payment_order::*;
+
+mod payment_order_received;
+pub use payment_order_received::*;
---
+++ src/payment_order/payment_order_received.rs
@@ -0,0 +1,13 @@
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_sb_entity_protobuf_model(topic_id = "payment-order-received")]
+pub struct PaymentOrderReceivedSbModel {
+    #[prost(string, tag = "1")]
+    pub order_id: String,
+    #[prost(int32, tag = "2")]
+    pub status: i32,
+    #[prost(string, tag = "4")]
+    pub psp_order_id: String,
+    #[prost(string, tag = "5")]
+    pub psp_name: String,
+}
