a49db213fd9eda8adee4c7a4aa225da06a1af0a8
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2025-11-18 20:42:45 +0200

    fix: add personal data.

--- src/account_registration/account_registration.rs
+++ src/account_registration/account_registration.rs
@@ -20,6 +20,45 @@ pub struct RegisteredSbModel {
     #[prost(string, tag = "4")]
     pub tmp_password: String,
 
+    #[prost(string, tag = "5")]
+    pub personal_data: PersonalDataSbModel,
+}
+
+pub struct PersonalDataSbModel {
+    #[prost(message, tag = "1")]
+    pub id: String,
+    #[prost(string, tag = "2")]
+    pub email: Optional<String>,
+    #[prost(string, tag = "3")]
+    pub first_name: Optional<String>,
+    #[prost(string, tag = "4")]
+    pub last_name: Optional<String>,
+    #[prost(string, tag = "5")]
+    pub city: Optional<String>,
+    #[prost(string, tag = "6")]
+    pub date_of_birth: Optional<String>,
+    #[prost(string, tag = "7")]
+    pub country_of_residence: Optional<String>,
+    #[prost(string, tag = "8")]
+    pub country_of_citizenship: Optional<String>,
+    #[prost(string, tag = "9")]
+    pub zip_code: Optional<String>,
+    #[prost(string, tag = "10")]
+    pub phone: Optional<String>,
+    #[prost(string, tag = "11")]
+    pub address: Optional<String>,
+    #[prost(string, tag = "12")]
+    pub sex: Optional<String>,
+    #[prost(string, tag = "13")]
+    pub account_type: Optional<String>,
+    #[prost(string, tag = "14")]
+    pub created_date: Optional<String>,
+    #[prost(string, tag = "15")]
+    pub region: Optional<String>,
+    #[prost(string, tag = "16")]
+    pub country_of_registration: Optional<String>,
+    #[prost(string, tag = "17")]
+    pub country_of_registration_by_ip: Optional<String>,
 }
 
 // Sanitized versions for logging
