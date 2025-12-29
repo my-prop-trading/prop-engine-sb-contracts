d09cbe61d6b2c6a62a13eba67fafb7a6ad6f6ab5
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2025-12-29 17:48:16 +0200

    fix: add fix for build.

--- src/kyc_webhook_update/contract_created.rs
+++ src/kyc_webhook_update/contract_created.rs
@@ -1,23 +1,22 @@
-use crate::common::AccountType; 
-
 service_sdk::macros::use_my_sb_entity_protobuf_model!();
-#[derive(Clone, PartialEq, ::prost::Message)]
+#[allow(clippy::derive_partial_eq_without_eq)]
 #[my_sb_entity_protobuf_model(topic_id = "contract-created")]
-pub struct ContractCreatedSbModel { // contract = contractor
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ContractCreatedSbModel {
     #[prost(string, tag = "1")]
     pub id: String,
     #[prost(string, tag = "2")]
     pub trading_package_id: String,
-    #[prost(i64, tag = "3")]
+    #[prost(int64, tag = "3")]
     pub contract_id: i64,
     #[prost(string, tag = "4")]
     pub client_id: String,
     #[prost(string, tag = "5")]
     pub trader_account_aggregated_id: String,
-    #[prost(enumeration="ClientLiveAccountContractStatus", tag = "6")]
-    pub sign_status: Option<ContractSignStatusDto>,
+    #[prost(enumeration="ContractSignStatusDto", tag = "6")]
+    pub sign_status: i32, 
     #[prost(enumeration="ContractLiveAccountStatusDto", tag = "7")]
-    pub live_account_status: ContractLiveAccountStatusDto,
+    pub live_account_status: i32,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
