57d03c1cb466f8621b711b07fcbca7b367005e4f
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2026-03-03 14:49:07 +0200

    fix: for prev commit.

--- src/account_email_changed/account_email_changed.rs
+++ src/account_email_changed/account_email_changed.rs
@@ -3,11 +3,11 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 #[my_sb_entity_protobuf_model(topic_id = "account-email-changed")]
 pub struct AccountEmailChangedSbModel {
     #[prost(message, tag = "1")]
-    pub event: Option<AccountEmailChangedSbModel>,
+    pub event: Option<EmailChangedSbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct AccountEmailChangedSbModel {
+pub struct EmailChangedSbModel {
     #[prost(string, tag = "1")]
     pub trader_id: String,
 
