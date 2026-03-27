ce6810395df905b4d6a17911626471c3843572b7
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2026-03-27 21:06:01 +0200

    feat: add rise event

---
+++ src/rise_status_update/mod.rs
@@ -0,0 +1,3 @@
+mod rise_status_update;
+
+pub use rise_status_update::*;
---
+++ src/rise_status_update/rise_status_update.rs
@@ -0,0 +1,19 @@
+service_sdk::macros::use_my_sb_entity_protobuf_model!();
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_sb_entity_protobuf_model(topic_id = "account-email-changed")]
+pub struct RiseStatusUpdatedSbModel {
+    #[prost(message, tag = "1")]
+    pub event: Option<AccountEmailChangedSbModel>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ARiseStatusUpdatedEventSbModel {
+    #[prost(string, tag = "1")]
+    pub rise_id: String,
+
+    #[prost(string, tag = "2")]
+    pub status: int32,
+
+    #[prost(string, tag = "3")]
+    pub email: String,
+}
