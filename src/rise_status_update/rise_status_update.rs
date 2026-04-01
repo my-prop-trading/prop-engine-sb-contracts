21872c120c385575483c102f0309e35f3e3b6547
MichaelRedkinFintechb2b <m.redkin@fintechb2b.com>
2026-04-01 11:14:39 +0300

    fix: one more

--- src/rise_status_update/rise_status_update.rs
+++ src/rise_status_update/rise_status_update.rs
@@ -3,11 +3,11 @@ service_sdk::macros::use_my_sb_entity_protobuf_model!();
 #[my_sb_entity_protobuf_model(topic_id = "rise-status-updated")]
 pub struct RiseStatusUpdatedSbModel {
     #[prost(message, tag = "1")]
-    pub event: Option<AccountEmailChangedSbModel>,
+    pub event: Option<RiseStatusUpdatedEventSbModel>,
 }
 
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct ARiseStatusUpdatedEventSbModel {
+pub struct RiseStatusUpdatedEventSbModel {
     #[prost(string, tag = "1")]
     pub rise_id: String,
 
